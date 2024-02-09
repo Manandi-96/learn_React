a99a16229c6944f86163a28d2c89d2e3233ef9c3
Manandi-96 <130781450+Manandi-96@users.noreply.github.com>
2024-02-09 17:35:22 +0530

    answers to question in level 2

--- Day-6/day6/src/index.js
+++ Day-6/day6/src/index.js
@@ -90,3 +90,186 @@ const App = () => {
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
+
+LEVEL 2-EX2
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// creating a color
+const generateHexColor = () => {
+  const str = '0123456789abcdef';
+  let color = '';
+
+  for (let i = 0; i < 6; i++) {
+    const index = Math.floor(Math.random() * str.length);
+    color += str[index];
+  }
+
+  return '#' + color;
+};
+
+// adding that color to an array
+const ColorList = () => {
+  const colors = [];
+
+  for (let i = 0; i < 32; i++) {
+    const hexaColor = generateHexColor();
+    colors.push(hexaColor);
+  }
+  console.log(colors);
+  return colors;
+};
+
+// display the list using map
+
+const Colors = ({ colors }) => {
+  const style = {
+        display:'grid',
+        gridTemplateColumns:'repeat(5,1fr)', 
+        gap: '1px'
+  };
+
+  const colorList = colors.map((color, index) => (
+    <div key={index} style={{ ...style, backgroundColor: color }}>
+      <Color bgColor={color} />
+    </div>
+  ));
+  console.log(colorList);
+  return <div>{colorList}</div>;
+};
+
+// adding the background to the color component as same as the value of the color
+const Color = ({ bgColor }) => {
+  const numberStyle = {
+    backgroundColor: bgColor,
+    padding: '10px',
+    borderRadius: '5px',
+    color: 'white',
+  };
+
+  return (
+    <div style={numberStyle}>
+      <h1>{bgColor}</h1>
+    </div>
+  );
+};
+
+const App = () => {
+  const container = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center', 
+  };
+
+  const colors = ColorList(); // defining colors variable here
+
+  return (
+    <div style={container}>
+      <div>
+        <h1>30 DAYS OF REACT</h1>
+        <h3>Colors</h3>
+        <ul>
+          <Colors colors={colors} />
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById('root');
+// we render the App component using the ReactDOM package
+ReactDOM.render(<App />, rootElement);
+
+// Question 2
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// creating a color
+const generateHexColor = () => {
+  const str = '0123456789abcdef';
+  let color = '';
+
+  for (let i = 0; i < 6; i++) {
+    const index = Math.floor(Math.random() * str.length);
+    color += str[index];
+  }
+
+  return '#' + color;
+};
+
+// adding that color to an array
+const ColorList = () => {
+  const colors = [];
+
+  for (let i = 0; i < 32; i++) {
+    const hexaColor = generateHexColor();
+    colors.push(hexaColor);
+  }
+  return colors;
+};
+
+// display the list using map
+
+const Colors = ({ colors }) => {
+
+  const colorList = colors.map((color, index) => (
+
+    <div key={index} style={{ width: '150px', height: '150px',passing:'10px',margin:'5px', backgroundColor: color }}>
+      <Color bgColor={color} />
+    </div>
+  ));
+
+  return (
+    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(8, 1fr)', gap: '1px' }}>
+      {colorList}
+    </div>
+  );
+};
+
+// adding the background to the color component as same as the value of the color
+const Color = ({ bgColor }) => {
+  const numberStyle = {
+    backgroundColor: bgColor,
+    padding: '10px',
+    borderRadius: '5px',
+    color: 'white',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    width: '100%',
+  };
+
+  return (
+    <div style={numberStyle}>
+      <h1>{bgColor}</h1>
+    </div>
+  );
+};
+
+const App = () => {
+  const container = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center', 
+  };
+
+  const colors = ColorList(); // defining colors variable here
+
+  return (
+    <div style={container}>
+      <div>
+        <h1>30 DAYS OF REACT</h1>
+        <h3>Colors</h3>
+        <Colors colors={colors} />
+      </div>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById('root');
+// we render the App component using the ReactDOM package
+ReactDOM.render(<App />, rootElement);
